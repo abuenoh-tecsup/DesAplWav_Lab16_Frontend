9d13cad434ff83383cd1482473896a909f3a053e
AlvaroJBH <alvarobuenoh@gmail.com>
2025-12-05 20:28:40 -0500

    Variable de entorno con nombre corregido

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4000";
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
 
 export async function apiClient(path: string, options: RequestInit = {}) {
   return fetch(`${BACKEND_URL}/api${path}`, {
